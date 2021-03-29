83dfa1fbc2c7a26c5500a3d12cad01badaacb6f0
cauezitto <caue.matheus.granrodeo@gmail>
2021-03-28 23:56:58 -0300

    trocando icone

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,9 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Reuniôes Barcelos Leite</title>
-        <meta property="og:image" content="/img/favicon.png"></meta>
-        <link rel="shortcut icon" href="/img/favicon.png" />
-        <link rel="apple-touch-icon" href="/img/favicon.png" />
+        <meta property="og:image" content="/img/logo.png"></meta>
+        <link rel="shortcut icon" href="/img/logo.png" />
+        <link rel="apple-touch-icon" href="/img/logo.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#4a6da7" />
         <meta
